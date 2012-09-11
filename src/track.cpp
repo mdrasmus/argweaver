ad75d8276dc51a9d31110d18f4e37dc64301094f
mdr229 <mdr229@v4linuxlogin1.cac.cornell.edu>
2012-09-11 14:55:50 -0400

    fixed track bug

--- src/track.cpp
+++ src/track.cpp
@@ -7,34 +7,6 @@ namespace arghmm {
 
 
 template <>
-bool read_track_line<double>(const char *line, 
-                     string &chrom, int &start, int &end, double &value)
-{
-    const int CHROM_NAME_LENGTH = 1000;
-    char chrom2[CHROM_NAME_LENGTH+1];
-
-    if (sscanf("%1000s\t%d\t%d\t%lf\n", chrom2, &start, &end, &value) != 4)
-        return false;
-    chrom = string(chrom2);
-    return true;
-}
-
-
-template <>
-bool read_track_line<int>(const char *line, 
-                     string &chrom, int &start, int &end, int &value)
-{
-    const int CHROM_NAME_LENGTH = 1000;
-    char chrom2[CHROM_NAME_LENGTH+1];
-
-    if (sscanf("%1000s\t%d\t%d\t%d\n", chrom2, &start, &end, &value) != 4)
-        return false;
-    chrom = string(chrom2);
-    return true;
-}
-
-
-template <>
 bool read_track_line<double>(const char *line, RegionValue<double> &region)
 {
     const int CHROM_NAME_LENGTH = 1000;
