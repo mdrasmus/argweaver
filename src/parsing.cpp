f9adaa76cd4cbdc7dd607315799f07b3742522a2
Matt Rasmussen <matt.rasmus@gmail.com>
2014-05-18 18:32:22 -0700

    move quote_arg to a parsing file

--- src/parsing.cpp
+++ src/parsing.cpp
@@ -139,5 +139,30 @@ char *concat_strs(char **strs, int nstrs)
 }
 
 
+// Quote an argument for use in a command shell
+// Argument is wrapped in single quotes and every single quote is escaped.
+string quote_arg(string text)
+{
+    int j = 0;
+    char text2[text.size() * 4 + 3];
+    text2[j++] = '\'';
+
+    for (unsigned int i=0; i<text.size(); i++) {
+        if (text[i] == '\'') {
+            text2[j++] = '\'';
+            text2[j++] = '\\';
+            text2[j++] = '\'';
+            text2[j++] = '\'';
+        } else
+            text2[j++] = text[i];
+    }
+
+    // terminate string
+    text2[j++] = '\'';
+    text2[j++] = '\0';
+
+    return string(text2);
+}
+
 
 } // namespace argweaver
