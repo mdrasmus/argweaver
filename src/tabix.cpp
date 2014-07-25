4e33984ab99c66cb08e3c90b41b1016d784ef67a
Melissa Hubisz <mjhubisz@gmail.com>
2014-07-25 15:42:44 -0400

    small changes to tabix.cpp,tabix.h

--- src/tabix.cpp
+++ src/tabix.cpp
@@ -13,31 +13,19 @@ using namespace std;
 
 FILE *read_tabix(const char *filename, const char *region,
                  const char *tabix_dir) {
-    //    bool exists = !access(filename, F_OK);
-    //    char index_file[strlen(filename)+5];
     FILE *pipe;
-    /*    if (!exists) {
-        fprintf(stderr, "%s not found\n", filename);
-        return NULL;
-        }*/
 
     if (region == NULL) {
         return read_compress(filename);
     }
 
-    /*    sprintf(index_file, "%s.tbi", filename);
-          exists = !access(index_file, F_OK);
-    if (!exists) {
-        fprintf(stderr, "%s does not have index file. Try running tabix on your file first.\n",
-                filename);
-        return NULL;
-        }*/
     string cmd = "tabix -h " + quote_arg(filename) + " " +  region;
     if (tabix_dir != NULL)
         cmd = string(tabix_dir) + "/" + cmd;
     pipe = popen(cmd.c_str(), "r");
     if (pipe == NULL) {
-        fprintf(stderr, "Error opening %s with tabix. Is tabix installed and in your PATH?\n",
+        fprintf(stderr, "Error opening %s with tabix. Is tabix installed and"
+                " in your PATH?\n",
                 filename);
         exit(1);
     }
